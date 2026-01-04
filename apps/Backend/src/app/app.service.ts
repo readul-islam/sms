af2171388b7c6a11e1932a96879849cb1301f0f1
readul-islam <developer.readul@gmail.com>
2026-01-04 17:13:30 +0600

    fix: the issue

--- apps/Backend/src/app/app.service.ts
+++ apps/Backend/src/app/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 @Injectable()
 export class AppService {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
   getData(): { message: string } {
+    console.log(this.connection.name);
     return { message: 'Hello API' };
   }
 }
