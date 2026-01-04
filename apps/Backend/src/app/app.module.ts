4bf96105614d414793c3bfaa810ca8587e3bf36b
readul-islam <developer.readul@gmail.com>
2026-01-04 16:45:21 +0600

    fix: the issue

--- apps/Backend/src/app/app.module.ts
+++ apps/Backend/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri:
+          configService.get<string>('MONGO_URI') ||
+          'mongodb://user:password@localhost:27017/sms?authSource=admin',
+      }),
+      inject: [ConfigService],
+    }),
     AuthModule,
-    MongooseModule.forRoot(process.env.MONGO_URI as string),
   ],
   controllers: [AppController],
   providers: [AppService],
